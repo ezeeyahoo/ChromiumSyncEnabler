47e2db6ba10e115db5e6509a53dbeefaef39c73d
Eshant Gupta <guptaeshant@gmail.com>
2017-12-05 11:07:50 +0530

    No more relevant

--- replace_chromium.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mv /Applications/Chromium.app/Contents/MacOS/Chromium /Applications/Chromium.app/Contents/MacOS/Chromium_bin
-
-cp -p /Users//Library/Mobile\ Documents/com\~apple\~CloudDocs/UnixScripts/Chromium /Applications/Chromium.app/Contents/MacOS/Chromium
